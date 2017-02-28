464a0e676c33e2c26d1ded9720e6efb10b3cad7b
unknown <>
2017-02-28 19:01:34 +0900

    initial commit

---
+++ app/src/main/java/com/lifeistech/developer/hide/memopad/MainActivity.java
@@ -0,0 +1,36 @@
+package com.lifeistech.developer.hide.memopad;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    boolean isFirst=true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences pref=getSharedPreferences("key_tutorial",Context.MODE_PRIVATE);
+        isFirst=pref.getBoolean("key_tutorial",true);
+
+        if (isFirst){
+            Intent intent=new Intent(this,ExplainActivity.class);
+            startActivity(intent);
+            SharedPreferences pref2=getSharedPreferences("key_tutorial", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor=pref2.edit();
+            editor.putBoolean("key_tutorial",false);
+            editor.commit();
+        }
+
+    }
+
+    public void memo(View v){
+        Intent intent=new Intent(this,MemoActivity.class);
+        startActivity(intent);
+    }
+}
