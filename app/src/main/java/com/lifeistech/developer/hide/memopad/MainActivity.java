3b293b2bea36370f8fc702d2c6a1e33cd327a299
unknown <>
2017-03-07 20:46:52 +0900

    ＋ボタン追加

--- app/src/main/java/com/lifeistech/developer/hide/memopad/MainActivity.java
+++ app/src/main/java/com/lifeistech/developer/hide/memopad/MainActivity.java
@@ -3,6 +3,7 @@ package com.lifeistech.developer.hide.memopad;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter<String> adapter;
     HashMap<String, String> hashMap;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +29,14 @@ public class MainActivity extends AppCompatActivity {
         //contentsNumberというkeyで保存されているメモの数の情報を取得(例５）
         SharedPreferences pref = getSharedPreferences("MemoPad", MODE_PRIVATE);
         int contentsNumber = pref.getInt("contentsNumber", 0);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),NewMemoActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
         //for文でgetSharedPreferences("0～5",Mode_private)を呼んで、そのなかのkey_titleの情報をtitleArrayに追加する
